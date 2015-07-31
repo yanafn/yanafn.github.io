3ad8cc30b13efd3c3cca23da10de63d89e4abb22
Mark Cornick <mark@markcornick.com>
2015-07-31 11:37:18 -0400

    minify the hell out of this

--- ninja.js
+++ ninja.js
@@ -1,10 +1,1 @@
-$(document).ready(function() {
-  $('a#button-yes').click(function(event) {
-    $('div#jumbotron-question').hide();
-    return $('div#jumbotron-yes').show();
-  });
-  return $('a#button-no').click(function(event) {
-    $('div#jumbotron-question').hide();
-    return $('div#jumbotron-no').show();
-  });
-});
+$(document).ready(function(){$("a#button-yes").click(function(event){$("div#jumbotron-question").hide();return $("div#jumbotron-yes").show()});return $("a#button-no").click(function(event){$("div#jumbotron-question").hide();return $("div#jumbotron-no").show()})});
