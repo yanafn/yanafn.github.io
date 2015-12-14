4ab9da1da51a47c59f2f416c7d0c3881881537a4
Mark Cornick <mark@markcornick.com>
2015-12-14 15:00:12 -0500

    Un-minify

--- ninja.js
+++ ninja.js
@@ -1,1 +1,11 @@
-$(document).ready(function(){$("a#button-yes").click(function(event){$("div#jumbotron-question").hide();return $("div#jumbotron-yes").show()});return $("a#button-no").click(function(event){$("div#jumbotron-question").hide();return $("div#jumbotron-no").show()})});
+/* eslint-env jquery */
+$(document).ready(function () {
+  $('a#button-yes').click(function (event) {
+    $('div#jumbotron-question').hide()
+    $('div#jumbotron-yes').show()
+  })
+  $('a#button-no').click(function (event) {
+    $('div#jumbotron-question').hide()
+    $('div#jumbotron-no').show()
+  })
+})
